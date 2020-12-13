e335beb8a8d3cf30704f741f402751e1d4b2cf94
mahdi-jafaree <mehdi.jafaree1375@gmail.com>
2020-12-13 07:13:02 +0430

    Initial commit

---
+++ src/main/java/mahdi/workspring/HelloMaven.java
@@ -0,0 +1,11 @@
+package mahdi.workspring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloMaven {
+    private final static Logger logger = LoggerFactory.getLogger(HelloMaven.class);
+    public static void main(String[] args) {
+        logger.info("It is my logger");
+    }
+}
